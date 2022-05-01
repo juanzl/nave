d74a52bee9e6dc0671baa2dee25d90146715ade5
juanzl <juancarloslizarragaencinas@MacBook-Pro-de-Juan.local>
2022-05-01 09:47:22 -0700

    Nuevas pantallas

---
+++ app/src/main/java/fragments/ActividadesFragment.java
@@ -0,0 +1,21 @@
+package fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import lizarraga.juan.nave.R;
+
+public class ActividadesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_actividades, container, false);
+    }
+}
+
---
+++ app/src/main/java/fragments/FantasiaFragment.java
@@ -0,0 +1,23 @@
+package fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import lizarraga.juan.nave.R;
+
+public class FantasiaFragment extends Fragment {
+
+    Button btn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+}
